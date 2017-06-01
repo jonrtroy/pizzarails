8bfe13f2ecc08e4daf10f796feb9278b785ce98e
Jonathan Troy <jonrtroy@gmail.com>
2017-06-01 15:00:45 -0400

    delete yelp.rb

--- pizzarails/config/initializers/yelp.rb
+++
@@ -1,4 +0,0 @@
-# Yelp.client.configure do |config|
-#   config.consumer_key = "yw5BueizFQMwPpgAFcaeCg"
-#   config.consumer_secret = "nA5IWvETkIaSYQeiuFGGCuxRb1QZXM8KiDTqkhzHeVsxzrGctSdddiQWqE7D01RL"
-# end
